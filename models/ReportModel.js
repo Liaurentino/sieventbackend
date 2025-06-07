9b650b403f56e4028281e5142f485362b722a91f
Liaurentino <liaurentinoneo25@gmail.com>
2025-06-07 13:00:25 +0700

    Fixing Bug Report

--- models/ReportModel.js
+++ models/ReportModel.js
@@ -1,8 +1,8 @@
 import mongoose from 'mongoose';
 
 const reportSchema = new mongoose.Schema({
-    reporter: { type: mongoose.Schema.Types.ObjectId, ref: 'user', required: true },
-    reported: { type: mongoose.Schema.Types.ObjectId, ref: 'user', required: true },
+    reporter: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    reported: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
     description: { type: String, required: true },
     createdAt: { type: Date, default: Date.now }
 });
